8f019413ee2b7de9a5ad4f23b8fa00f1ba45356f
HiblAntonio <74968697+HiblAntonio@users.noreply.github.com>
2024-02-05 15:09:32 +0100

    Add files via upload

---
+++ Day 1 - Project One/MonoPraksa11/MonoPraksa11/Classes/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoPraksa11.Classes
+{
+    internal class SavingsAccount : BankAccount
+    {
+        private float intrestRate = 1.07f;
+        public SavingsAccount(float balance, string owner) : base(balance, owner) { }
+
+        public override void WithdrawMoney(float amount)
+        {
+            if ((Balance - amount) < 0)
+                Console.WriteLine("Not enough funds!");
+            else
+            {
+                Balance -= amount;
+                RecordTransaction(amount, "Withdraw");
+            }
+        }
+
+        public override void ApplyIntrestRate()
+        {
+            Balance *= intrestRate;
+        }
+    }
+}
