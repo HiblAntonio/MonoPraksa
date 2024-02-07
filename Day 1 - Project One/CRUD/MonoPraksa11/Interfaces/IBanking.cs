202d9c99d694dce10ff9cad7fa977f59940ee9d5
HiblAntonio <antoniohibl@gmail.com>
2024-02-07 15:08:26 +0100

    Changed file name

---
+++ Day 1 - Project One/CRUD/MonoPraksa11/Interfaces/IBanking.cs
@@ -0,0 +1,20 @@
+﻿using MonoPraksa11.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoPraksa11
+{
+    internal interface IBanking
+    {
+        string FindTransaction(int transactionNumber);
+        void DepositMoney(float value);
+        void WithdrawMoney(float value);
+        void ApplyIntrestRate();
+        void CheckBalance();
+        void CheckOwner();
+        void DisplayTransactionHistory();
+    }
+}
