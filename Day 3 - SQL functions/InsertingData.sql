d48fb3502a39de2b7a80e54e2dfbbdc8a436b053
HiblAntonio <antoniohibl@gmail.com>
2024-02-07 16:18:43 +0100

    Creating a DB, inserting and manipulating data

--- Day 3 - SQL functions/InsertingData.sql
+++ Day 3 - SQL functions/InsertingData.sql
@@ -1,65 +1,61 @@
--- Insert data into Customer table
-INSERT INTO "Customer" ("Firstname", "Lastname", "Dob") 
-VALUES 
-    ('John', 'Doe', '1990-01-01 00:00:00'),
-    ('Jane', 'Smith', '1985-05-15 00:00:00'),
-    ('Michael', 'Johnson', '1988-07-20 00:00:00'),
-    ('Emily', 'Brown', '1992-09-10 00:00:00'),
-    ('David', 'Wilson', '1979-03-25 00:00:00');
+-- Customer Table Examples
+INSERT INTO "Customer" ("Id", "Firstname", "Lastname", "Dob")
+VALUES
+    ('6f9619ff-8b86-d011-b42d-00c04fc964ff', 'John', 'Doe', '1990-01-01 00:00:00'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc965ff', 'Jane', 'Smith', '1985-05-15 00:00:00'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc966ff', 'Michael', 'Johnson', '1988-07-20 00:00:00'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc967ff', 'Emily', 'Brown', '1992-09-10 00:00:00'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc968ff', 'David', 'Wilson', '1979-03-25 00:00:00');
 
--- Insert data into Ticket table
-INSERT INTO "Ticket" ("CustomerId", "Price", "SeatNumber") 
-VALUES 
-    (1, 50.00, 101),
-    (2, 45.00, 102),
-    (3, 55.00, 103),
-    (4, 60.00, 104),
-    (5, 40.00, 105);
+-- Ticket Table Examples
+INSERT INTO "Ticket" ("Id", "CustomerId", "Price", "SeatNumber")
+VALUES
+    ('6f9619ff-8b86-d011-b42d-00c04fc974ff', '6f9619ff-8b86-d011-b42d-00c04fc964ff' , 50.00, 101),
+    ('6f9619ff-8b86-d011-b42d-00c04fc975ff', '6f9619ff-8b86-d011-b42d-00c04fc964ff' , 45.00, 102),
+    ('6f9619ff-8b86-d011-b42d-00c04fc976ff', '6f9619ff-8b86-d011-b42d-00c04fc964ff', 55.00, 103),
+    ('6f9619ff-8b86-d011-b42d-00c04fc977ff', '6f9619ff-8b86-d011-b42d-00c04fc964ff', 60.00, 104),
+    ('6f9619ff-8b86-d011-b42d-00c04fc978ff', '6f9619ff-8b86-d011-b42d-00c04fc964ff', 40.00, 105);
 
--- Insert data into Theater table
-INSERT INTO "Theater" ("Address", "Capacity", "Size") 
-VALUES 
-    ('123 Main St', 200, 1500.00),
-    ('456 Broadway', 300, 2000.00),
-    ('789 Elm St', 250, 1800.00),
-    ('101 Pine St', 400, 2500.00),
-    ('202 Oak St', 350, 2200.00);
+-- Theater Table Examples
+INSERT INTO "Theater" ("Id", "Address", "Capacity", "Size")
+VALUES
+    ('6f9619ff-8b86-d011-b42d-00c04fc984ff', '123 Main St', 200, 1500.00),
+    ('6f9619ff-8b86-d011-b42d-00c04fc985ff', '456 Broadway', 300, 2000.00),
+    ('6f9619ff-8b86-d011-b42d-00c04fc986ff', '789 Elm St', 250, 1800.00),
+    ('6f9619ff-8b86-d011-b42d-00c04fc987ff', '101 Pine St', 400, 2500.00),
+    ('6f9619ff-8b86-d011-b42d-00c04fc988ff', '202 Oak St', 350, 2200.00);
 
--- Insert data into Actor table
-INSERT INTO "Actor" ("FirstName", "LastName", "Dob", "Salary") 
-VALUES 
-    ('Tom', 'Hanks', '1956-07-09 00:00:00', 1000000.00),
-    ('Meryl', 'Streep', '1949-06-22 00:00:00', 1500000.00),
-    ('Leonardo', 'DiCaprio', '1974-11-11 00:00:00', 2000000.00),
-    ('Angelina', 'Jolie', '1975-06-04 00:00:00', 1800000.00),
-    ('Brad', 'Pitt', '1963-12-18 00:00:00', 1900000.00);
+-- Actor Table Examples
+INSERT INTO "Actor" ("Id", "FirstName", "LastName", "Dob", "Salary")
+VALUES
+    ('6f9619ff-8b86-d011-b42d-00c04fc994ff', 'Tom', 'Hanks', '1956-07-09 00:00:00', 1000000.00),
+    ('6f9619ff-8b86-d011-b42d-00c04fc995ff', 'Meryl', 'Streep', '1949-06-22 00:00:00', 1500000.00),
+    ('6f9619ff-8b86-d011-b42d-00c04fc996ff', 'Leonardo', 'DiCaprio', '1974-11-11 00:00:00', 2000000.00),
+    ('6f9619ff-8b86-d011-b42d-00c04fc997ff', 'Angelina', 'Jolie', '1975-06-04 00:00:00', 1800000.00),
+    ('6f9619ff-8b86-d011-b42d-00c04fc998ff', 'Brad', 'Pitt', '1963-12-18 00:00:00', 1900000.00);
 
--- Insert data into Genre table
-INSERT INTO "Genre" ("Type") 
-VALUES 
-    ('Drama'),
-    ('Comedy'),
-    ('Action'),
-    ('Romance'),
-    ('Thriller');
+-- Genre Table Examples
+INSERT INTO "Genre" ("Id", "Type")
+VALUES
+    ('6f9619ff-8b86-d011-b42d-00c04fc9a4ff', 'Drama'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc9a5ff', 'Comedy'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc9a6ff', 'Action'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc9a7ff', 'Romance'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc9a8ff', 'Thriller');
 
--- Insert data into Play table
-INSERT INTO "Play" ("GenreId", "Title") 
-VALUES 
-    (1, 'Hamlet'),
-    (2, 'The Importance of Being Earnest'),
-    (3, 'Inception'),
-    (4, 'Titanic'),
-    (5, 'The Dark Knight');
+-- Play Table Examples
+INSERT INTO "Play" ("Id", "GenreId", "Title")
+VALUES
+    ('6f9619ff-8b86-d011-b42d-00c04fc9b4ff', '6f9619ff-8b86-d011-b42d-00c04fc9a4ff', 'Hamlet'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc9b5ff', '6f9619ff-8b86-d011-b42d-00c04fc9a4ff', 'The Importance of Being Earnest'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc9b6ff', '6f9619ff-8b86-d011-b42d-00c04fc9a4ff', 'Inception'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc9b7ff', '6f9619ff-8b86-d011-b42d-00c04fc9a4ff', 'Titanic'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc9b8ff', '6f9619ff-8b86-d011-b42d-00c04fc9a4ff', 'The Dark Knight');
 
--- Insert data into Performance table
-INSERT INTO "Performance" ("ActorId", "TheaterId", "PlayId", "Time", "Date") 
-VALUES 
-    (1, 1, 1, '2024-02-06 19:00:00', '2024-02-06'),
-    (2, 2, 2, '2024-02-07 19:30:00', '2024-02-07'),
-    (3, 3, 3, '2024-02-08 20:00:00', '2024-02-08'),
-    (4, 4, 4, '2024-02-09 18:45:00', '2024-02-09'),
-    (5, 5, 5, '2024-02-10 21:15:00', '2024-02-10');
+INSERT INTO "Performance" ("Id", "ActorId", "TheaterId", "PlayId", "Time", "Date")
+VALUES
+    ('6f9619ff-8b86-d011-b42d-00c04fc9c4ff', '6f9619ff-8b86-d011-b42d-00c04fc994ff', '6f9619ff-8b86-d011-b42d-00c04fc984ff', '6f9619ff-8b86-d011-b42d-00c04fc9b4ff', '14:00:00', '2024-02-01'),
+    ('6f9619ff-8b86-d011-b42d-00c04fc9c5ff', '6f9619ff-8b86-d011-b42d-00c04fc994ff', '6f9619ff-8b86-d011-b42d-00c04fc984ff', '6f9619ff-8b86-d011-b42d-00c04fc9b4ff', '16:30:00', '2024-02-02');
 
 CREATE TABLE "Example"(
 	"Id" INT PRIMARY KEY,
--- Day 3 - SQL functions/Theater.sql
+++ Day 3 - SQL functions/Theater.sql
@@ -1,27 +1,27 @@
 CREATE TABLE "Customer"(
-	"Id" SERIAL PRIMARY KEY,
+	"Id" UUID PRIMARY KEY,
 	"Firstname" VARCHAR(50) NOT NULL,
 	"Lastname" VARCHAR(50) NOT NULL,
 	"Dob" TIMESTAMP
 );
 
 CREATE TABLE "Ticket"(
-	"Id" SERIAL PRIMARY KEY,
-	"CustomerId" INT NOT NULL,
-	CONSTRAIN "FK_Ticket_Customer_CustomerId" FOREIGN KEY ("CustomerId") REFERENCES "Customer"("Id"),
+	"Id" UUID PRIMARY KEY,
+	"CustomerId" UUID NOT NULL,
+	CONSTRAINT "FK_Ticket_Customer_CustomerId" FOREIGN KEY ("CustomerId") REFERENCES "Customer"("Id"),
 	"Price" FLOAT NOT NULL,
 	"SeatNumber" INT UNIQUE NOT NULL
 );
 
 CREATE TABLE "Theater"(
-	"Id" SERIAL PRIMARY KEY,
+	"Id" UUID PRIMARY KEY,
 	"Address" VARCHAR(100) NOT NULL,
 	"Capacity" INT,
 	"Size" FLOAT
 );
 
 CREATE TABLE "Actor"(
-	"Id" SERIAL PRIMARY KEY,
+	"Id" UUID PRIMARY KEY,
 	"FirstName" VARCHAR(100) NOT NULL,
 	"LastName" VARCHAR(100) NOT NULL,
 	"Dob" TIMESTAMP,
@@ -29,25 +29,25 @@ CREATE TABLE "Actor"(
 );
 
 CREATE TABLE "Genre"(
-	"Id" SERIAL INT PRIMARY KEY,
+	"Id" UUID PRIMARY KEY,
 	"Type" VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE "Play"(
-	"Id" SERIAL PRIMARY KEY,
-	"GenreId" INT NOT NULL,
-	CONSTRAIN "FK_Play_Genre_GenreId" FOREIGN KEY ("GenreId") REFERENCES "Genre"("Id"),
+	"Id" UUID PRIMARY KEY,
+	"GenreId" UUID NOT NULL,
+	CONSTRAINT "FK_Play_Genre_GenreId" FOREIGN KEY ("GenreId") REFERENCES "Genre"("Id"),
 	"Title" VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE "Performance"(
-	"Id" SERIAL PRIMARY KEY,
-	"ActorId" INT NOT NULL,
-	"TheaterId" INT NOT NULL,
-	"PlayId" INT NOT NULL,
-	CONSTRAIN ormance_Actor_ActorId" FOREIGN KEY ("ActorId") REFERENCES "Actor"("Id"),
-	CONSTRAIN "FK_Performance_Theater_TheaterId" FOREIGN KEY ("TheaterId") REFERENCES "Theater"("Id"),
-	CONSTRAIN "FK_Performance_Play_PlayId" FOREIGN KEY ("PlayId") REFERENCES "Play"("Id"),
+	"Id" UUID PRIMARY KEY,
+	"ActorId" UUID NOT NULL,
+	"TheaterId" UUID NOT NULL,
+	"PlayId" UUID NOT NULL,
+	CONSTRAINT "Performance_Actor_ActorId" FOREIGN KEY ("ActorId") REFERENCES "Actor"("Id"),
+	CONSTRAINT "FK_Performance_Theater_TheaterId" FOREIGN KEY ("TheaterId") REFERENCES "Theater"("Id"),
+	CONSTRAINT "FK_Performance_Play_PlayId" FOREIGN KEY ("PlayId") REFERENCES "Play"("Id"),
 	"Time" TIMESTAMP NOT NULL,
 	"Date" DATE NOT NULL
 );
